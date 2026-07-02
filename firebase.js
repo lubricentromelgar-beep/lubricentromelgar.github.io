4d71db96b1a83afa4aeec1b4f480bb7e1f697a85
lubricentromelgar-beep <lubricentromelgar@gmail.com>
2026-07-02 12:48:10 -0600

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// ════════════════════════════════════════════════════════════════
+//  FIREBASE CONFIG
+//  Reemplaza estos valores con los de tu proyecto en Firebase Console
+//  https://console.firebase.google.com
+// ════════════════════════════════════════════════════════════════
+
+const firebaseConfig = {
+  apiKey:            "AIzaSyDO9jZmr6rCOacHoMpqH7ow3BZ9wgkJLa0",
+  authDomain:        "lubricentro-mlegar.firebaseapp.com",
+  projectId:         "lubricentro-mlegar",
+  storageBucket:     "lubricentro-mlegar.firebasestorage.app",
+  messagingSenderId: "337653204601",
+  appId:             "1:337653204601:web:f2ea4c84b659cda6b9f6a8"
+};
